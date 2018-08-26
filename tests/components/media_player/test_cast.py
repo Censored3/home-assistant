c0fd4dc822578eb5d666e3c300607f2594e65162
Fréderic Kinnaer <frederic@censored.be>
2018-08-26 19:18:27 +0200

    Fix testclass

--- tests/components/media_player/test_cast.py
+++ tests/components/media_player/test_cast.py
@@ -247,7 +247,7 @@ async def test_normal_raises_platform_not_ready(hass):
     """Test cast platform raises PlatformNotReady if HTTP dial fails."""
     with patch('pychromecast.dial.get_device_status', return_value=None):
         with pytest.raises(PlatformNotReady):
-            await async_setup_cast(hass, {'host': 'host1'})
+            await async_setup_cast(hass, {'host': 'host1', 'port': 8009})
 
 
 async def test_replay_past_chromecasts(hass):
